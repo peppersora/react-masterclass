3d68deb7f6b5e1d4dd50f9aadd1152fe1824ab9a
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-12 22:28:23 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import styled,{keyframes} from "styled-components";
-// styled의 component를 이용한다.
-//  중요한것은 반드시 백틱을 사용하고 백틱내부에 css내용을 적는다.
-
-const Title = styled.h1`
-  color:${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-display: flex;
-height:100vh;
-width: 100vw;
-justify-content: center;
-align-items: center;
-background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-        <Title>Hello</Title> 
-    </Wrapper>
+  <div>
+    <Circle bgColor="teal"/>
+    <Circle bgColor="tomato"/>
+  </div>
   );
 }
 
