58ac70b0a452dad09eb045441d56e8bf39d34ca1
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-14 15:34:59 +0900

    ver5

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { useState } from "react";
+
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onchange = (event: React.FormEvent<HTMLInputElement>) =>{
-    const {
-      currentTarget:{value},
-    } = event;
-    setValue(value);
-  };
-  // event의 any type이란 any => 말그대로 아무타입이나 될 수 있다는 뜻
-  // 하지만 우리는 any를 지양해야한다. 항상 타입스크립트에게 무슨
-  // 타입인지를 쓰거나 설명하고자 노력해야함
-  const onsubmit = (event: React.FormEvent<HTMLFormElement>) =>{
-    event.preventDefault();
-    console.log("hello", value);
-  }
   return (
-  <div>
-    <form onSubmit={onsubmit}>
-      <input value={value} 
-      onChange={onchange}
-      type="text" 
-      placeholder="username" />
-      <button>Log in</button>
-    </form>
-  </div>
+    <Router/>
   );
 }
 
