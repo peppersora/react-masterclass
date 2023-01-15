24c5105d810b5fe7b045d7bb6949e48233c68580
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-15 22:59:40 +0900

    css

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,19 @@
 import {BrowserRouter, Route, Routes,} from "react-router-dom";
+import Chart from "./routes/Chart";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
+import Price from "./routes/Price";
 
 function Router(){
     return(
         <BrowserRouter>
             <Routes> 
-            <Route path="/" element={<Coins/>}>
-                 
-                </Route>       
-            </Routes>
-
-            <Routes> 
-            <Route path="/:coinId" element={<Coin/>}>
-                
-                </Route>
-            </Routes>
-
-       
+            <Route path="/" element={<Coins/>}/>       
+            <Route path="/:coinId" element={<Coin/>}/>  
+            <Route path="chart" element={<Chart/>}/>
+            <Route path="price" element={<Price/>}/> 
+            
+            </Routes>   
         </BrowserRouter>
     );
 }
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,9 @@
 
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Route, Routes, useLocation, useParams,Outlet } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 
 const Container = styled.div`
@@ -26,42 +28,165 @@ const Loader = styled.span`
     display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 
 interface Routestate {
     state:string;
     }
+
+// interface ITag{
+//     coin_counter: number;
+//     ico_counter: number;
+//     id: string;
+//     name: string;
+// }
+
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    // tag: ITag[];
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+      USD: {
+        ath_date: string;
+        ath_price: number;
+        market_cap: number;
+        market_cap_change_24h: number;
+        percent_change_1h: number;
+        percent_change_1y: number;
+        percent_change_6h: number;
+        percent_change_7d: number;
+        percent_change_12h: number;
+        percent_change_15m: number;
+        percent_change_24h: number;
+        percent_change_30d: number;
+        percent_change_30m: number;
+        percent_from_price_ath: number;
+        price: number;
+        volume_24h: number;
+        volume_24h_change_24h: number;
+      };
+    };
+}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams();
     // param은 타입입력안해도 v6부터 자동으로 된다고...
-    // console.log(coinId);
-    const {state} = useLocation() as Routestate;
-    // console.log(state);
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const {state} = useLocation() as Routestate; 
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
 
     useEffect(() => {
         (async() => {
             const infoData= await (
             await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
             ).json();
-            console.log(info);
+            // console.log(info);
                 // json 1개 끝
             const priceData = await(
             await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
-            console.log(priceInfo);
+            // console.log(priceInfo);
                 setInfo(infoData);
                 setPriceInfo(priceData);
+                setLoading(false);
                
         })();
-    },[]);
+    },[coinId]);
+    // coinId가 변한다면 [] 안에 코드는 다시 실행될 것이다.
+    /*  우리는 coinId가 변하지 않는다는것을 알기 때문에
+        [] 이렇게 해도 상관없다. coinId는 state를 사용하지 않기
+        때문에*/
     return(
         <Container>
         <Header>
-            <Title>{state ? state : "Loading..." }</Title>
+            <Title>
+                {state ? state : "Loading..." }
+                </Title>
             </Header>
-            {loading ? (<Loader>Loading...</Loader>) :null}
+                {loading ? (
+            <Loader>Loading...</Loader>
+            ) : (
+            <>
+            <Outlet/>
+            <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+            </Overview>
+        </>
+      )}
             </Container>
     );
 }
